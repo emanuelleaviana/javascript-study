a404edacd1b52ab3cc199441094cb657e0ad2f6a
Emanuelle Viana <manuavbr@gmail.com>
2022-04-28 19:36:29 -0300

    update ex13 e 14

---
+++ exercícios javascript/aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+
+do {
+    console.log(`Estamos no passo ${c}`)
+    c++
+} while (c <=3)
+
+
+
+/* var i = 1
+
+while (i <= 5) {
+    console.log(`teste passo ${i}`)
+    i++
+}
+*/
---
+++ exercícios javascript/aula14/teste.js
@@ -0,0 +1,6 @@
+var c = 1
+
+while (c<=3) {
+    console.log(c)
+    c++
+}
